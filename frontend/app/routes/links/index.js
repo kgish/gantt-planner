9f63b9d267c589c84705d2b5cbb4ee1034c28327
Kiffin Gish <kiffin.gish@planet.nl>
2017-05-17 10:29:43 +0200

    Bootbox alert.

--- frontend/app/routes/links/index.js
+++ frontend/app/routes/links/index.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    remove: function(model) {
-      if(confirm('Are you sure?')) {
-        model.destroyRecord();
-      }
+    actions: {
+        remove: function(model) {
+            bootbox.confirm({
+                message: 'Are you sure you want to delete this link?',
+                callback: function(result) {
+                    if (result) {
+                        model.destroyRecord();
+                    }
+                }
+            });
+        }
+    },
+    model: function() {
+        return this.store.findAll('link');
     }
-  },
-  model: function() {
-    return this.store.findAll('link');
-  }
 });
--- frontend/app/routes/tasks/index.js
+++ frontend/app/routes/tasks/index.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    remove: function(model) {
-      if(confirm('Are you sure?')) {
-        model.destroyRecord();
-      }
+    actions: {
+        remove: function(model) {
+            bootbox.confirm({
+                message: 'Are you sure you want to delete this task?',
+                callback: function(result) {
+                    if (result) {
+                        model.destroyRecord();
+                    }
+                }
+            });
+        }
+    },
+    model: function() {
+        return this.store.findAll('task');
     }
-  },
-  model: function() {
-    return this.store.findAll('task');
-  }
 });
