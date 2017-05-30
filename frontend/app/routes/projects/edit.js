9aedc8d5a2dc3a08328d1a69ed3e0d78bde30453
Kiffin Gish <kiffin.gish@planet.nl>
2017-05-30 21:08:44 +0200

    Gantt chart access database (no longer hard-coded).

--- frontend/app/routes/projects/edit.js
+++ frontend/app/routes/projects/edit.js
@@ -1,3 +1,4 @@
+/*eslint no-console: ["error", { allow: ["log", "warn", "error"] }] */
 import Ember from 'ember';
 import SaveModelMixin from 'frontend/mixins/projects/save-model-mixin';
 
@@ -17,38 +18,44 @@ export default Ember.Route.extend(SaveModelMixin, {
             links = models.links,
             gantt_tasks = { data: [], links: [] };
         if (project) {
-            gantt_tasks.data.push({
-                id:         project.get('id').toString(),
+            let entry = {
+                id:         project.get('id'),
                 text:       project.get('text'),
                 start_date: moment(project.get('start_date')).format('DD-MM-YYYY'),
                 duration:   project.get('duration'),
                 parent:     '0',
                 progress:   project.get('progress'),
                 open:       true
-
-            });
+            };
+            console.log('project='+JSON.stringify(entry));
+            gantt_tasks.data.push(entry);
             if (tasks && tasks.length) {
                 tasks.forEach(task => {
-                    gantt_tasks.data.push({
-                        id:         task.get('id').toString(),
+                    let entry = {
+                        id:         task.get('id'),
                         text:       task.get('text'),
                         start_date: moment(task.get('start_date')).format('DD-MM-YYYY'),
                         duration:   task.get('duration'),
-                        parent:     task.get('parent').toString(),
+                        parent:     task.get('parent'),
                         progress:   task.get('progress'),
                         open:       true
-                    });
+
+                    };
+                    console.log('task='+JSON.stringify(entry));
+                    gantt_tasks.data.push(entry);
                 });
             }
 
             if (links && links.length) {
                 links.forEach(link => {
-                    gantt_tasks.links.push({
-                        id:      link.get('id').toString(),
-                        source:  link.get('source').toString(),
-                        target:  link.get('target').toString(),
+                    let entry = {
+                        id:      link.get('id'),
+                        source:  link.get('source'),
+                        target:  link.get('target'),
                         type:    link.get('code')
-                    });
+                    };
+                    console.log('link='+JSON.stringify(link));
+                    gantt_tasks.links.push(entry);
                 });
             }
          }
